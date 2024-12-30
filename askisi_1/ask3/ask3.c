b544ad8c661d35b4053161d2ba39bb551106dba6
Dista01 <staa.dista@yahoo.gr>
2024-12-30 11:01:13 +0200

    askisi_1 semi-final
    Co-authored-by: Panagiotis Kalozoumis <PKalozoumis@users.noreply.github.com>

---
+++ askisi_1/ask3/ask3.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <omp.h>
+#include <sys/time.h>
+#include <stdlib.h>
+
+extern double work(int i);
+
+double getTime(void)
+{
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return (double)t.tv_sec + (double)t.tv_usec * 1.0e-6;
+}
+
+void initialize(double *A, int N)
+{
+#pragma omp parallel
+#pragma omp single
+  {
+    double t = getTime();
+    for (int i = 0; i < N; i++)
+    {
+#pragma omp task firstprivate(i) shared(A)
+      {
+        printf("Task %d is being executed by thread %d\n", i, omp_get_thread_num());
+
+        A[i] = work(i);
+      }
+    }
+
+#pragma omp taskwait
+    printf("%lf\n", getTime() - t);
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  omp_set_dynamic(0);
+  omp_set_num_threads(1);
+  int N = 100;
+  double *A = (double *)malloc(N * sizeof(double));
+
+  initialize(A, N);
+
+  return 0;
+}
---
+++ askisi_1/ask3/work.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+double work(int workload)
+{
+  unsigned long j;
+
+  double a = (double)workload;
+
+  for (j = 0; j < 10000000; j++)
+  {
+    a += sqrt(1.1) * sqrt(1.2) * sqrt(1.3) * sqrt(1.4) * sqrt(1.5);
+    a += sqrt(1.6) * sqrt(1.7) * sqrt(1.8) * sqrt(1.9) * sqrt(2.0);
+    a += sqrt(1.1) * sqrt(1.2) * sqrt(1.3) * sqrt(1.4) * sqrt(1.5);
+    a += sqrt(1.6) * sqrt(1.7) * sqrt(1.8) * sqrt(1.9);
+  }
+
+  return a;
+}
