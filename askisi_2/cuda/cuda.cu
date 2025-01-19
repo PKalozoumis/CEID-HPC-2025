29113520524e846fa4570ae48799c22833589b15
PKalozoumis <panagiotis.kalozoumis@gmail.com>
2025-01-19 12:01:15 +0200

    GPU done
    Co-authored-by: Dimitris Stasinos <Dista01@users.noreply.github.com>

--- askisi_2/cuda/cuda.cu
+++ askisi_2/cuda/cuda.cu
@@ -1,21 +1,47 @@
 #include <stdio.h>
 #include <omp.h>
 #include <cuda.h>
+#include <sys/time.h>
 
-void initialize_matrix(float** matrix, int N)
+//========================================================================================================
+
+void alloc_2d(float*** A, int N)
 {
-    srand(time(NULL) + 1000*omp_get_thread_num());
-    //printf("%d\n", omp_get_thread_num());
+    *A = (float **)malloc(N * sizeof(float *));
 
-    *matrix = (float*)malloc(N*N*sizeof(float));
+    for (int i = 0; i < N; i++)
+    {
+        (*A)[i] = (float*)malloc(N * sizeof(float));
+    }
+}
 
-    for (int i = 0; i < N*N; i++)
+//========================================================================================================
+
+void initialize_matrix(float **matrix, int N)
+{
+    srand(time(NULL) + 1000 * omp_get_thread_num());
+    // printf("%d\n", omp_get_thread_num());
+
+    *matrix = (float *)malloc(N * N * sizeof(float));
+
+    for (int i = 0; i < N * N; i++)
     {
         (*matrix)[i] = (rand() / (float)RAND_MAX) * 10;
     }
 }
 
-void print_matrix(float* matrix, int N)
+//========================================================================================================
+
+double get_wtime()
+{
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return t.tv_sec + t.tv_usec * 1e-6;
+}
+
+//========================================================================================================
+
+void print_matrix(float *matrix, int N)
 {
 
     printf("==================================================================\n");
@@ -24,9 +50,9 @@ void print_matrix(float* matrix, int N)
     {
         for (int j = 0; j < N; j++)
         {
-            printf("%.02f", matrix[i*N + j]);
+            printf("%.02f", matrix[i * N + j]);
 
-            if (j < N-1)
+            if (j < N - 1)
             {
                 printf("\t");
             }
@@ -35,22 +61,222 @@ void print_matrix(float* matrix, int N)
     }
 }
 
+void print_matrix_cpu(float** matrix, int N)
+{
+    printf("==================================================================\n");
 
-__global__ void multiply_matrix(float* R, float* M1, float* M2, int N)
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%.02f", matrix[i][j]);
+
+            if (j < N - 1)
+            {
+                printf("\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+//========================================================================================================
+
+__global__ void multiply_matrix(float *R, float *M1, float *M2, int N)
 {
     int i = blockIdx.y * blockDim.y + threadIdx.y;
     int j = blockIdx.x * blockDim.x + threadIdx.x;
 
-    //printf("(%d, %d)\n", i, j);
+    // printf("(%d, %d)\n", i, j);
 
     if (i < N && j < N)
     {
         for (int k = 0; k < N; k++)
-            R[i*N + j] += M1[i*N + k]*M2[k*N + j];
+            R[i * N + j] += M1[i * N + k] * M2[k * N + j];
     }
 }
 
-int main(int argc, char** argv)
+//========================================================================================================
+
+__global__ void add_matrix(float *R, float *M1, float *M2, int N)
+{
+    int i = blockIdx.y * blockDim.y + threadIdx.y;
+    int j = blockIdx.x * blockDim.x + threadIdx.x;
+    int pos = i * N + j;
+
+    if (i < N && j < N)
+    {
+        R[pos] = M1[pos] + M2[pos];
+    }
+}
+
+//========================================================================================================
+
+__global__ void sub_matrix(float *R, float *M1, float *M2, int N)
+{
+    int i = blockIdx.y * blockDim.y + threadIdx.y;
+    int j = blockIdx.x * blockDim.x + threadIdx.x;
+    int pos = i * N + j;
+
+    if (i < N && j < N)
+    {
+        R[pos] = M1[pos] - M2[pos];
+    }
+}
+
+//========================================================================================================
+
+void cpu_matrix_add(float **AB, float **CD, float **result ,int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i][j] = AB[i][j] + CD[i][j];
+        }
+    }
+}
+
+//========================================================================================================
+
+void cpu_matrix_sub(float **AB, float **CD, float **result ,int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i][j] = AB[i][j] - CD[i][j];
+        }
+    }
+}
+
+//========================================================================================================
+
+void cpu_matrix_mull(float *A, float *B, float** result, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i][j] = 0;
+            for (int t = 0; t < n; t++)
+            {
+                result[i][j] += A[i * n + t] * B[t * n + j];
+            }
+        }
+    }
+}
+
+//========================================================================================================
+
+void free_memMatrix(float **matrix, int n){
+    for(int i=0;i<n;i++){
+        free(matrix[i]);
+    }
+
+    free(matrix);
+}
+
+//========================================================================================================
+
+void cpu_calculation(float *A, float *B, float *C, float *D, int n, float** E, float** F)
+{
+
+    float **AC, **BD, **AD, **BC;
+
+    AC = (float **)malloc(n * sizeof(float *));
+    BD = (float **)malloc(n * sizeof(float *));
+    AD = (float **)malloc(n * sizeof(float *));
+    BC = (float **)malloc(n * sizeof(float *));
+
+    for (int i = 0; i < n; i++)
+    {
+        AC[i] = (float *)malloc(n * sizeof(float));
+        BD[i] = (float *)malloc(n * sizeof(float));
+        AD[i] = (float *)malloc(n * sizeof(float));
+        BC[i] = (float *)malloc(n * sizeof(float));
+    }
+
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            #pragma omp task
+            cpu_matrix_mull(A, C, AC, n);
+
+            #pragma omp task
+            cpu_matrix_mull(B, D, BD, n);
+
+            #pragma omp task
+            cpu_matrix_mull(A, D, AD, n);
+
+            #pragma omp task
+            cpu_matrix_mull(B, C, BC, n);
+        }
+    }
+
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            #pragma omp task
+            cpu_matrix_sub(AC, BD, E, n);
+
+            #pragma omp task
+            cpu_matrix_add(AD, BC, F, n);
+        }
+    }
+
+     #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            #pragma omp task
+            free_memMatrix(AC, n);
+
+            #pragma omp task
+            free_memMatrix(BD, n);
+            
+            #pragma omp task
+            free_memMatrix(AD, n);
+
+            #pragma omp task
+            free_memMatrix(BC, n);
+        }
+    }
+}
+
+//========================================================================================================
+
+void matrix_comparison(float **cpuE, float **cpuF,float* gpuE, float* gpuF, int n){
+    
+    int error=0;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(cpuE[i][j]!=gpuE[i*n+j] || cpuF[i][j]!=gpuF[i*n+j]){
+                error=1;
+                break;
+            }
+        }
+        if(error==1){
+            break;
+        }
+    }
+
+    if(error==0){
+        printf("Successful comparison\n");
+    }else{
+        printf("Comparison failed\n");
+    }
+    
+}
+
+//========================================================================================================
+
+int main(int argc, char **argv)
 {
     if (argc == 1)
     {
@@ -60,7 +286,7 @@ int main(int argc, char** argv)
 
     int N = atoi(argv[1]);
 
-    //Initialize matrices in host
+    // Initialize matrices in host
     //===========================================================================
 
     float *A, *B, *C, *D;
@@ -93,33 +319,98 @@ int main(int argc, char** argv)
     printf("\nD\n");
     print_matrix(D, N);*/
 
-    float *devA, *devB, *devC, *devD, *devR;
+    float *devA, *devB, *devC, *devD, *devAC, *devBD, *devAD, *devBC;
 
-    int arraySize = N*N*sizeof(float);
+    int arraySize = N * N * sizeof(float);
 
     cudaMalloc(&devA, arraySize);
     cudaMalloc(&devB, arraySize);
     cudaMalloc(&devC, arraySize);
     cudaMalloc(&devD, arraySize);
-    cudaMalloc(&devR, arraySize);
-    cudaMemset(devR, 0, arraySize);
+    cudaMalloc(&devAC, arraySize);
+    cudaMalloc(&devBD, arraySize);
+    cudaMalloc(&devAD, arraySize);
+    cudaMalloc(&devBC, arraySize);
+
+    cudaMemset(devAC, 0, arraySize);
+    cudaMemset(devBD, 0, arraySize);
+    cudaMemset(devAD, 0, arraySize);
+    cudaMemset(devBC, 0, arraySize);
 
     cudaMemcpy(devA, A, arraySize, cudaMemcpyHostToDevice);
     cudaMemcpy(devB, B, arraySize, cudaMemcpyHostToDevice);
     cudaMemcpy(devC, C, arraySize, cudaMemcpyHostToDevice);
     cudaMemcpy(devD, D, arraySize, cudaMemcpyHostToDevice);
 
-    print_matrix(A, N);
-    print_matrix(B, N);
-
     int blockSize = 16;
-    dim3 block(blockSize, blockSize); //16x16 = 256 threads per block. A multiple of 32, the warp size
+    dim3 block(blockSize, blockSize); // 16x16 = 256 threads per block. A multiple of 32, the warp size
     dim3 grid((N + blockSize - 1) / blockSize, (N + blockSize - 1) / blockSize);
 
-    multiply_matrix<<<grid, block>>>(devR, devA, devB, N);
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    
+    cudaEventRecord(start, 0);
 
-    float* result = (float*)malloc(arraySize);
-    cudaMemcpy(result, devR, arraySize, cudaMemcpyDeviceToHost);
+    multiply_matrix<<<grid, block>>>(devAC, devA, devC, N);
+    multiply_matrix<<<grid, block>>>(devBD, devB, devD, N);
+    multiply_matrix<<<grid, block>>>(devAD, devA, devD, N);
+    multiply_matrix<<<grid, block>>>(devBC, devB, devC, N);
 
-    print_matrix(result, N);
+    sub_matrix<<<grid, block>>>(devAC, devAC, devBD, N);
+    add_matrix<<<grid, block>>>(devAD, devAD, devBC, N);
+
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+
+    printf("Total time for GPU calculations: %.03lfs\n", milliseconds/1000);
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+    // Get back final results
+
+    float *E = (float *)malloc(arraySize);
+    float *F = (float *)malloc(arraySize);
+
+    cudaMemcpy(E, devAC, arraySize, cudaMemcpyDeviceToHost);
+    cudaMemcpy(F, devAD, arraySize, cudaMemcpyDeviceToHost);
+
+    //print_matrix(E, N);
+    //print_matrix(F, N);
+
+    // Verify results
+    //-------------------------------------------------------------------------------
+    float **Echeck, **Fcheck;
+    alloc_2d(&Echeck, N);
+    alloc_2d(&Fcheck, N);
+
+    cpu_calculation(A, B, C, D, N, Echeck, Fcheck);
+
+    //print_matrix_cpu(Echeck, N);
+    //print_matrix_cpu(Fcheck, N);
+
+    matrix_comparison(Echeck,Fcheck,E,F,N);
+
+    // Free memory
+
+    cudaFree(devA);
+    cudaFree(devB);
+    cudaFree(devC);
+    cudaFree(devD);
+    cudaFree(devAC);
+    cudaFree(devBD);
+    cudaFree(devAD);
+    cudaFree(devBC);
+
+    free(A);
+    free(B);
+    free(C);
+    free(D);
+
+    free_memMatrix(Echeck, N);
+    free_memMatrix(Fcheck, N);
 }
